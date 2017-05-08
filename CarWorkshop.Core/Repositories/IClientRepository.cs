f6db3e6d00690d7d599726c2291bd67260bd27e7
Szymon Kalista <szymon.przemyslaw.kalista@gmail.com>
2017-05-08 20:39:08 +0200

    Created interfaces for Client and Employee

--- CarWorkshop.Core/Repositories/IClientRepository.cs
+++ CarWorkshop.Core/Repositories/IClientRepository.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CarWorkshop.Core.Models;
 
 namespace CarWorkshop.Core.Repositories
 {
     public interface IClientRepository
     {
+        Client GetClientById(int Id);
+
+        Client GetClientByEmail(string email);
+
+        IEnumerable<Client> GetAllClients();
+
+        void AddClient(Client client);
+
+        void RemoveClient(int clientId);
+
+        void UpdateClient(Client client);
+
     }
 }
---
+++ CarWorkshop.Core/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarWorkshop.Core.Models;
+
+namespace CarWorkshop.Core.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployeeById(int Id);
+
+        Employee GetEmployeeByEmail(string email);
+
+        IEnumerable<Employee> GetAllEmployees();
+
+        void AddEmployee(Employee employee);
+
+        void RemoveEmployee(int Id);
+
+        void UpdateEmployee(Employee employee);
+
+    }
+}

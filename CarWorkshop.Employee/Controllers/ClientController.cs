5e3ad3fc1504a123c6d38b0625eeac97b077193a
Szymon Kalista <szymon.przemyslaw.kalista@gmail.com>
2017-07-26 12:41:31 +0200

    Stubs for actions in client controller created

--- CarWorkshop.Employee/Controllers/ClientController.cs
+++ CarWorkshop.Employee/Controllers/ClientController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CarWorkshop.Infrastructure.DTO;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,36 @@ namespace CarWorkshop.Employee.Controllers
     {
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ClientDTO model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ClientDTO model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int Id)
         {
             return View();
         }

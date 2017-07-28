fdb9dff970baf43210ea2038bf8afd60cac49870
Szymon Kalista <szymon.przemyslaw.kalista@gmail.com>
2017-07-28 11:51:13 +0200

    buttons fixed

--- CarWorkshop.Employee/Controllers/CarController.cs
+++ CarWorkshop.Employee/Controllers/CarController.cs
@@ -3,16 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using CarWorkshop.Infrastructure.DTO;
+using CarWorkshop.Infrastructure.Services;
 
 namespace CarWorkshop.Employee.Controllers
 {
     public class CarController : Controller
     {
+        private readonly ICarService _service;
+        public CarController(ICarService service)
+        {
+            _service = service;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CarDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.AddCar(model);
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            CarDTO car = await _service.GetCar(Id);
+
+            return View(car);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CarDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.UpdateCar(model);
+
+                return RedirectToAction("Index");
+            }
+
             return View();
         }
     }

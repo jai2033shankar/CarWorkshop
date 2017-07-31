276e39c2523e8b5e9338d9acdb97e20521cfad1a
Szymon Kalista <szymon.przemyslaw.kalista@gmail.com>
2017-07-31 22:20:31 +0200

    Stubs for actions in repair controller added

--- CarWorkshop.Employee/Controllers/RepairController.cs
+++ CarWorkshop.Employee/Controllers/RepairController.cs
@@ -3,14 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CarWorkshop.Infrastructure.DTO;
 
 namespace CarWorkshop.Employee.Controllers
 {
     public class RepairController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(RepairDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                // ... 
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            // ...
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RepairDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                // ... 
+                return RedirectToAction("Index");
+            }
+
             return View();
         }
     }

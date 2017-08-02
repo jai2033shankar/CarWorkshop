bd82818ff644083442d121b6edf5436173449583
Szymon Kalista <szymon.przemyslaw.kalista@gmail.com>
2017-08-02 22:26:28 +0200

    employee info now shows in the profile

--- CarWorkshop.Employee/Controllers/ProfileController.cs
+++ CarWorkshop.Employee/Controllers/ProfileController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CarWorkshop.Infrastructure.Services;
+using CarWorkshop.Infrastructure.DTO;
+using System.Security.Claims;
 
 namespace CarWorkshop.Employee.Controllers
 {
     public class ProfileController : Controller
     {
-        
-        public IActionResult Index()
-        {
 
-            return View();
+        private readonly IEmployeeService _service;
+
+        public ProfileController(IEmployeeService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Add error checking.
+            string email = HttpContext.User.Claims.SingleOrDefault(x => x.Type == ClaimTypes.Email).Value;
+
+            EmployeeDTO employee = await _service.GetEmployee(email);
+
+            return View(employee);
         }
     }
 }
